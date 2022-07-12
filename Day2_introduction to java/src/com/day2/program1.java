10311d737e0e9c90d334fc39ac8006194951ce17
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-07-12 13:31:33 +0530

    First commit

---
+++ Day2_introduction to java/src/com/day2/program1.java
@@ -0,0 +1,17 @@
+package com.day2;
+import java.util.Scanner;
+public class program1 {
+
+	public static void main(String[] args) {
+//		int a=15;
+//		System.out.println("The value of a is: " +a);
+//		System.out.println("Welcom");
+		Scanner s1 = new Scanner(System.in);
+		double a= s1.nextDouble();
+		System.out.println("The value is "+a);
+		s1.close();
+		
+
+	}
+
+}
