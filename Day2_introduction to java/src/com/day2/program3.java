0c27041867efaefbae7baba93d4ebe7ddb34cc70
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-07-14 11:51:14 +0530

    commit 3

---
+++ Day2_introduction to java/src/com/day2/program3.java
@@ -0,0 +1,13 @@
+package com.day2;
+import java.util.Scanner;
+public class program3 {
+
+	public static void main(String[] args) {
+		Scanner h = new Scanner(System.in);
+		String i= h.next();
+		System.out.println("Good Morning "+i);
+		h.close();
+
+	}
+
+}
