8c0b86a0514931e9f0389312544e16b1de13fe31
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-07-17 12:44:11 +0530

    commi2

--- Day2_introduction to java/src/com/day2/program1.java
+++ Day2_introduction to java/src/com/day2/program1.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class program1 {
 
 	public static void main(String[] args) {
-//		int a=15;
-//		System.out.println("The value of a is: " +a);
-//		System.out.println("Welcom");
+		int a=15;
+		System.out.println("The value of a is: " +a);
+     	System.out.println("Welcom");
 		Scanner s1 = new Scanner(System.in);
-		double a= s1.nextDouble();
-		System.out.println("The value is "+a);
+		double s= s1.nextDouble();
+		System.out.println("The value is "+s);
 		s1.close();
 		
 
