d833404b3c8dc9354ae81fff3042e2d8036c1670
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-07-17 23:39:59 +0530

    commit2

---
+++ Day4_encapsulation,packages,abstraction,acess specifier/src/com/encapsulation/SBI.java
@@ -0,0 +1,12 @@
+package com.encapsulation;
+
+public class SBI {
+	private int ATM_PIN;
+	public int getATM_PIN(){
+		return ATM_PIN;
+	}
+	public void setATM_PIN(int PIN) {
+		ATM_PIN = PIN;
+	}
+
+}
