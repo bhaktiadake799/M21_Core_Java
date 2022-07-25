b985ff00c54c65cb8a1b2ae8413e7682e6b82dff
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-07-25 09:11:49 +0530

    commit 2

--- assignment2/src/com/ass/Main.java
+++ assignment2/src/com/ass/Main.java
@@ -1,2 +1,13 @@
 package com.ass;
 import java.util.Scanner;
+public class Main {
+	static int FindSum(int number)
+	{
+		return number == 0 ? 0: number %10 + FindSum(number /10);
+	}
+	public static void main(String[] args) {
+		int number=120;
+					System.out.println(FindSum(number));
+	}
+
+}
