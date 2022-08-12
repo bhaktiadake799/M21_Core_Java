ec42272fc8fe6d0827082d70e73623cded428d07
Bhakti Adake <Bhakti Adake@LAPTOP-9FE9G249>
2022-08-12 23:32:42 +0530

    commit2

---
+++ Day 17 Multithreding/src/com/cg/threding/program5.java
@@ -0,0 +1,19 @@
+package com.cg.threding;
+public class program5
+{
+public static void main(String args[])
+{
+Thread t = Thread.currentThread( );
+System.out.println("Current Thread :" + t);
+t.setName("Void");
+System.out.println("Current thread :" +t);
+try
+{
+	Thread.sleep(1);
+}
+catch (InterruptedException e)
+{
+	System.out.println("Main Thread Interrupted");
+		}
+	}
+}
